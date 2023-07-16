25b701d797508d6be11eb92250f637a7da6a0f1b
Jack Duignan <Jdu80@uclive.ac.nz>
2023-07-16 13:09:35 +1200

    Coninued finding fit

--- Data Processing/IRSensorFitting.py
+++ Data Processing/IRSensorFitting.py
@@ -8,6 +8,14 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 # sensor data
-sensor_distances = np.array([200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500])
+sensor_distances = np.array([200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100])
 sensor_rawData = np.array([580, 445, 340, 280, 245, 230, 214, 211, 210, 206])
 
+# fit the data
+a, b = np.polyfit(sensor_distances, sensor_rawData, 1)
+
+# plot the fit
+plt.plot(sensor_distances, sensor_rawData, 'bo', label='Raw Data')
+plt.plot(sensor_distances, a*sensor_distances + b, 'r-', label='Fit')
+plt.legend()
+plt.show()
