cc73183f005baeb3f3d71939ebda55427e58467a
Jack Duignan <Jdu80@uclive.ac.nz>
2023-08-10 09:19:55 +1200

    Started on return to base

--- main/returnToBase.cpp
+++ main/returnToBase.cpp
@@ -124,3 +124,115 @@ void homeReturn(RobotInfo_t* robotInfo) {
     //     homingState = "home";
     // }
 }
+
+static uint8_t state = 0;
+static int16_t baseHeading = 0;
+
+#define STATE_OFFSET 10 // Offset for state reporting
+#define TURN_DISTANCE 30 // The distance to turn when a wall is detected
+#define ROTATE_SPEED 30 // The degrees offset used to rotate the robot
+#define US_TOLERANCE 10 // The uncertainty of the ultrasonic sensor
+
+enum states {
+    HEAD_HOME,
+    HUG_WALL,
+    ROTATE_LEFT,
+    ROTATE_RIGHT,
+    AT_HOME,
+};
+
+enum wallSide {
+    UNKOWN,
+    LEFT,
+    RIGHT,
+};
+
+
+/**
+ * @brief Return to base state machine 
+ * @param robotInfo Pointer to the robot information struct
+ */
+void returnToBase(RobotInfo_t* robotInfo) {
+    // Variable definitions
+    static int16_t targetHeading = 0; // The heading try to follow
+    static bool firstRun = true; // Flag to indicate if the state has just been entered
+    static uint16_t previousUSDistance = 10000; // The previous ultrasonic distance
+    static uint8_t wallSide = 0; // The side of the wall the robot is following  
+
+    // State machine
+    switch (state) {
+        case HEAD_HOME: // Move towards the home base if there are no walls in the way
+            if (firstRun) {
+                firstRun = false; // Reset the first run flag
+            }
+
+            // Check if there is a wall in front of the robot
+            if (robotInfo->IRTop_Distance < TURN_DISTANCE) { // Object detected
+                state = ROTATE_RIGHT;
+                firstRun = true;
+            }
+                    
+            motors_followHeading(robotInfo, baseHeading, 30);
+            break;
+        
+        case HUG_WALL: // Follow the wall
+            if (firstRun) {
+                firstRun = false; // Reset the first run flag
+                targetHeading = robotInfo->IMU_Heading; // Set the target heading to the current heading
+            }
+
+            // Check if the robot is parallel to the wall and correct if it is not
+            if (wallSide == LEFT & robotInfo->USLeft_Distance > previousUSDistance + US_TOLERANCE) { // Drifting away
+                targetHeading -= 1;
+            } else if (wallSide == LEFT & robotInfo->USLeft_Distance < previousUSDistance - US_TOLERANCE) { // Drifting towards
+                targetHeading += 1;
+            } else if (wallSide == RIGHT & robotInfo->USRight_Distance > previousUSDistance + US_TOLERANCE) { // Drifting away
+                targetHeading += 1;
+            } else if (wallSide == RIGHT & robotInfo->USRight_Distance < previousUSDistance - US_TOLERANCE) { // Drifting towards
+                targetHeading -= 1;
+            }
+
+            // Check if there is a wall in front of the robot
+            if (wallSide == LEFT & robotInfo->IRTop_Distance < TURN_DISTANCE) { // Object detected
+                state = ROTATE_RIGHT;
+                firstRun = true;
+            } else if (wallSide == RIGHT & robotInfo->IRTop_Distance < TURN_DISTANCE) { // Object detected
+                state = ROTATE_LEFT;
+                firstRun = true;
+            }
+
+            motors_followHeading(robotInfo, targetHeading, 30);
+
+            break;
+
+        case ROTATE_LEFT: // Rotate left until the robot is facing the wall
+            if (firstRun) {
+                firstRun = false; // Reset the first run flag
+            }
+
+            // Check if the robot is parallel to the wall
+            if (robotInfo->USLeft_Distance > previousUSDistance) { // The robot is parallel to the wall
+                state = HUG_WALL;
+                firstRun = true;
+            } else {
+                // Rotate the robot on the spot left
+                motors_followHeading(robotInfo, robotInfo->IMU_Heading - ROTATE_SPEED, 0);
+            }
+            break;
+
+        case ROTATE_RIGHT: // Rotate right until the robot is facing the wall
+            if (firstRun) {
+                firstRun = false; // Reset the first run flag
+            }
+
+            // Check if the robot is parallel to the wall
+            if (robotInfo->USRight_Distance > previousUSDistance) { // The robot is parallel to the wall
+                state = HUG_WALL;
+                firstRun = true;
+            } else {
+                // Rotate the robot on the spot right
+                motors_followHeading(robotInfo, robotInfo->IMU_Heading + ROTATE_SPEED, 0);
+            }
+            break;
+    }
+}
